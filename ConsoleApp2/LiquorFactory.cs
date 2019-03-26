0408b03d8a4e172644d538d25c22e68a5bc3a512
Mansi Verma <manverma@microsoft.com>
2019-03-26 15:37:28 +0530

    added

--- ConsoleApp2/LiquorFactory.cs
+++ ConsoleApp2/LiquorFactory.cs
@@ -4,16 +4,11 @@ using System.Text;
 
 namespace ConsoleApp2
 {
-    public class FoodFactory : IItemFactory
+    class LiquorFactory : IItemFactory
     {
         public Item GetItemInstance(string itemId, string desc, double price, int quantity, ItemType itemType)
         {
-            if (ItemType.FOOD == itemType)
-            {
-                return new Chocolate(itemId, desc, price, quantity, itemType);
-            }
-
-            return null;
+            throw new NotImplementedException();
         }
     }
 }
